931dc21ee9aa708373d6ab1139d9c6202cf2e746
w314 <piroska804@revature.com>
2024-05-02 11:14:33 -0500

    feat: Added backend skeleton

---
+++ ers-backend/src/test/java/com/wp/ers/ErsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wp.ers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
