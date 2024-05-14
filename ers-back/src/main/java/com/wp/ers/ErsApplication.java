016bd1f5959a86d05c89b0f81a3ba3424401774f
w314 <piroska.werner@gmail.com>
2024-05-13 19:56:59 -0500

    Add annotations to ErsApplication

--- ers-back/src/main/java/com/wp/ers/ErsApplication.java
+++ ers-back/src/main/java/com/wp/ers/ErsApplication.java
@@ -2,7 +2,14 @@ package com.wp.ers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EntityScan("com.wp.ers.models") // tells Spring to look for DB entities in that package
+@ComponentScan("com.wp.ers") // tells Spring to look for beans
+@EnableJpaRepositories("com.wp.repositories") // tells Spring to look for repositories in that package
 @SpringBootApplication
 public class ErsApplication {
 
